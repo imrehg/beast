e6b631ca791165e860288b0594009441cd2f8510
Ronald McCollam <mccollam@gmail.com>
2016-08-16 09:40:39 -0400

    New image

--- start.sh
+++ start.sh
@@ -2,6 +2,6 @@
 
 echo "rendering image"
 # cat /usr/src/app/images/image.raw > /dev/fb1
-fbi -d /dev/fb1 -T 1 -noverbose -a images/fixed.png
+fbi -d /dev/fb1 -T 1 -noverbose -a images/bug.png
 
 sleep 9999999
