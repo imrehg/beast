5d1f4b8097d8bb27c53c7e31d6144530b4f2d045
Gergely Imreh <imrehg@gmail.com>
2016-08-20 16:06:38 +0800

    web images

--- start.sh
+++ start.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 echo "rendering image"
-fbi -d /dev/fb1 -T 1 -noverbose -a images/taipei_hackerspace.png
+if [ ${REMOTEIMG} ]; then
+  echo "Remote image from ${REMOTEIMG}"
+  FILENAME="${REMOTEIMG##*/}"
+  wget "$REMOTEIMG" -O "${FILENAME}"
+  fbi -d /dev/fb1 -T 1 -noverbose -a "${FILENAME}"
+else
+  fbi -d /dev/fb1 -T 1 -noverbose -a images/taipei_hackerspace.png
+fi
 
 sleep 9999999
