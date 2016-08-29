4bdb5584edcbdaccb6caf55cebb6940c759b7bfc
Gergely Imreh <imrehg@gmail.com>
2016-08-29 23:17:51 +0800

    add single resin image

--- start.sh
+++ start.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
 echo "rendering image"
-if [ ${REMOTEIMG} ]; then
-  echo "Remote image from ${REMOTEIMG}"
-  FILENAME="${REMOTEIMG##*/}"
-  wget "$REMOTEIMG" -O "${FILENAME}"
-  fbi -d /dev/fb1 -T 1 -noverbose -a "${FILENAME}"
-else
-  fbi -d /dev/fb1 -T 1 -noverbose -a images/taipei_hackerspace.png
-fi
+fbi -d /dev/fb1 -T 1 -noverbose -a images/resin_dark_full.png
 
 sleep 9999999
